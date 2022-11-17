7b3e13a46cd3f4f1d0b2cb33417f9763552ec860
ericrkuo <ericrkuo@gmail.com>
2022-11-17 00:30:58 -0800

    Small refactor

--- src/service/programSlicerService.py
+++ src/service/programSlicerService.py
@@ -16,53 +16,56 @@ class ProgramSlicerService:
 
         statement: ast.AST
         for statement in block.statements:
-                        
+
             if type(statement) is not ast.Assign:
                 continue
+            
+            self.analyzeAssign(state, statement)
 
-            # TODO extract out into its own analyze or analyzeAssign function and write tests
-            # we need to be careful that none of our future changes break existing behaviour
-            # analyze(σ, n, x=e)
-
-            # line number
-            n = statement.lineno
-
-            # targets can have length greater than one. For example, if we have a=b=1
-            targets = statement.targets
-            value = statement.value
-
-            # S_l = L(0) ∪ L(1) ∪ ... ∪ L(|L|-1)
-            S_l = set().union(*[list for list in state.L])
-
-            for target in targets:
-                
-                if type(target) is ast.Name:
-                
-                    x = target.id
-                    # S_e = M(y_0) ∪ M(y_1) ∪ ... ∪ M(y_n) where y_0,...,y_n are vars read in e
-                    # TODO what if array of vars (Just THINK of more cases)
-                    # TODO write tests for this, include array case
-                    varsRead = self.astUtils.getAllVariables(value)
-                    S_e = set().union(*[state.M.get(var, {}) for var in varsRead])
-                    state.M[x] = set().union({n}, S_e, S_l)
-                
-                # Handle special cases like (a,b) = (1,2)
-                elif type(target) is ast.Tuple and type(value) is ast.Tuple:
-
-                    assert len(target.elts) == len(value.elts) # TODO raise exception and add appropriate error message
-
-                    tNode: ast.Name
-                    for i, tNode in enumerate(target.elts):
-
-                        x = tNode.id
-                        vNode = value.elts[i]
-                        varsRead = self.astUtils.getAllVariables(vNode)
-                        S_e = set().union(*[state.M.get(var, {}) for var in varsRead])
-                        state.M[x] = set().union({n}, S_e, S_l)
-
-                else:
-                    print('UNEXPECTED')
-                    # TODO raise exception, or is there another case we're not considering??
+        # TODO handle loops, conditionals, etc.
 
         return state
+
+    # TODO document and write tests, we need to be careful that none of our future changes break existing behaviour
+    # # analyze(σ, n, x=e)
+    def analyzeAssign(self, state: AbstractState, statement: ast.Assign):
+         # line number
+        n = statement.lineno
+
+        # targets can have length greater than one. For example, if we have a=b=1
+        targets = statement.targets
+        value = statement.value
+
+        # S_l = L(0) ∪ L(1) ∪ ... ∪ L(|L|-1)
+        S_l = set().union(*[list for list in state.L])
+
+        for target in targets:
+            
+            if type(target) is ast.Name:
+            
+                x = target.id
+                # S_e = M(y_0) ∪ M(y_1) ∪ ... ∪ M(y_n) where y_0,...,y_n are vars read in e
+                # TODO what if array of vars (Just THINK of more cases)
+                # TODO write tests for this, include array case
+                varsRead = self.astUtils.getAllVariables(value)
+                S_e = set().union(*[state.M.get(var, {}) for var in varsRead])
+                state.M[x] = set().union({n}, S_e, S_l)
+            
+            # Handle special cases like (a,b) = (1,2)
+            elif type(target) is ast.Tuple and type(value) is ast.Tuple:
+
+                assert len(target.elts) == len(value.elts) # TODO raise exception and add appropriate error message
+
+                tNode: ast.Name
+                for i, tNode in enumerate(target.elts):
+
+                    x = tNode.id
+                    vNode = value.elts[i]
+                    varsRead = self.astUtils.getAllVariables(vNode)
+                    S_e = set().union(*[state.M.get(var, {}) for var in varsRead])
+                    state.M[x] = set().union({n}, S_e, S_l)
+
+            else:
+                print('UNEXPECTED')
+                # TODO raise exception, or is there another case we're not considering??
 

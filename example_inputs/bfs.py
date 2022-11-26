b60db8980ab78254863a5183eed382b284ee48ca
ericrkuo <ericrkuo@gmail.com>
2022-11-26 15:09:05 -0800

    keep old bfs

--- example_inputs/bfs.py
+++ example_inputs/bfs.py
@@ -1,28 +1,27 @@
-def BFS(n: int):
-    metrics.emit(BFSStartEvent(n))
-    logger.log("BFS triggered for node {n}")
-    span = tracer.spanBuilder("BFS")
-    parentSpan = span;
-    
-    nodes = [False for _ in range(node)]
-    a = 0;
-    nodes[n] = True
-    que.add(n)
-    
-    while len(que) != 0:
-        n = que.poll()
-        ## change Context.current().with() -> Context.current().of()
-        childSpan = tracer.spanBuilder("Node " + n).setParent(Context.current().of(parentSpan)).startSpan()
-        parentSpan = childSpan
-        for i in range(len(adj[n])):
-            a = adj[n].get(i)
-            if not nodes[a]:
-                nodes[a] = True
-                que.add(a)
-        childSpan.end()
-        if len(que) > PROGRAM_THRESHOLD:
-            metrices.emit(BFSQueueSizeExceededEvent(n, que))
-            logger.log("Queue size exceeded threshold of " + PROGRAM_THRESHOLD)
-    metrics.emit(BFSEndEvent(n))
-    logger.log("BFS finished for node {n}")
-    span.end()
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
+}
+
+visited = [] # List to keep track of visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+bfs(visited, graph, 'A') 
